cc4ca66fa28abe364efd40d13a4763f8ffc005cb
Luca Mazzilli <setmaat@gmail.com>
2021-10-09 09:05:06 +0200

    scraper remove and doc update

--- README.md
+++ README.md
@@ -17,11 +17,10 @@ You have to provide 5 arguments:
 - port
 - admin
 - password
-- number of zones/array of zones id (optional, default is 40, can be a number or an array of number)
 
 ```javascript
 const iAlarm = require('ialarm'); 
-const alarm = new iAlarm("192.168.1.81", "80", "myAdmin", "myPassword", [1,2,5,10,15]);
+const alarm = new iAlarm("192.168.1.81", "18034", "myAdmin", "myPassword", [1,2,5,10,15]);
 
 alarm.getStatus().then(function (response) {
     console.log('response: ' + JSON.stringify(response));
@@ -78,30 +77,58 @@ alarm.bypassZone(1, true).then(function (response) {
 ### functions and emitted events
 #### getStatus
 returns a promise with with the current status and an array of zone statuses
+```json
+{
+    "status": "ARMED_HOME", // ARMED_AWAY,ARMED_HOME,DISARMED,CANCEL,TRIGGERED
+    "zones": [
+        {
+            "lastChecked": "2021-10-09T06:45:52.770Z", //last checked 
+            "id": 1, //zone number
+            "zone": 1, //same as id
+            "name": "Ingresso", //zone friendly name
+            "status": 1, //tcp zone status (used to decode ok, alarm, bypass, lowbatt, fault, wirelessLoss, etc)
+            "inUse": true, //inUse=false means that the zone is disabled on the alarm 
+            "alarm": false, //sensor triggered
+            "bypass": false, //sensor bypassed
+            "lowbat": false, //low battery detected
+            "fault": false, //it means the sensors is open
+            "open": false, //it means the sensors is open
+            "wirelessLoss": false, //it means the alarm lost connection to this sensor
+            "ok": true, // if all the above are false
+            "problem": false, // just the negation of "ok" 
+            "message": "OK", //message decoded from "status" property
+            "typeId": 1, //zone type id
+            "type": "Ritardata",  //zone type name, decoded from typeId
+            "voiceId": 1, //alarm bell type
+            "voiceName": "Fisso" //alarm bell type name, decoded from voiceId
+        }
+    ]
+}
 ```
-{"zones":[{"id":1,"message":"OK"}],"status":"ARMED_HOME"}
-```
-- status: ARMED_AWAY,ARMED_HOME,DISARMED,CANCEL,TRIGGERED
-- zones: 
-  - id is the zone number on iAlarm 
-  - status is the original status code of web panel (0, 3, 8, 16, etc)
-  - ok is status = 0 and grabs from web panel this message: "OK";
-  - alarm is status & 3 and grabs from web panel this message: "zone alarm"
-  - open is status & 16 and grabs from web panel this message: "zone fault"
-  - lowbat is (status & 32)&&((status & 8)==0) and grabs from web panel this message: "wireless detector low battery"  
-  - fault is (status & 64)&&((status & 8)==0) and grabs from web panel this message: "wireless detector loss"  
-  - message is the web panel grabbed message for zone
-//TODO docs for more properties
 
 #### getEvents
 the last 100 events recorded in the host
-```
-{"date":"2018-11-11 07:25:04","zone":"70","message":"Sistema Disarmato"}
+```json
+{
+    "date": "2021-11-09T07:35:38.000Z",
+    "zone": 70,
+    "message": "Disarm report",
+    "description": "Disarm report (zone 70)"
+}
 ```
 #### getZoneInfo
-the configuration of the 40 zones (or configured number of zones) found on the host.
-```
-{ "1" : {"id":"1","name":"Porta","type":"Ritardato"}, "2" : {"id":"2","name":"Cucina","type":"Perimetrale"} }
+without arguments `getZoneInfo()`it returns the configuration of the zones found on the host. You can filter a single zone using argument 0: `getZoneInfo(1)`.
+ 
+```json
+    {
+        "id": 1, //zone number
+        "zone": 1, //same as id
+        "name": "Ingresso", //zone friendly name
+        "typeId": 1, //zone type id
+        "type": "Ritardata",  //zone type name, decoded from typeId
+        "voiceId": 1, //alarm bell type
+        "voiceName": "Fisso" //alarm bell type name, decoded from voiceId
+    }
 ```
 #### armAway
 arm in away mode the alarm, then return the status exposed by getStatus
@@ -121,7 +148,3 @@ bypass/remove bypass of a zone, then return the status exposed by getStatus
 
 #### filterStatusZones(zones)
 will filter zones with relevant event (zone alarm, bypass, errors, etc). The input must be an array of zones emitted with **getStatus**
-
-## Notes:
-1) some features like zone message are based on iAlarm js reverse enginering, so i haven't fully tested them.
-2) getAllZones is pretty slow, cause it post 40 requests. Provide a zone number to boost it a little.
--- ialarm.js
+++ ialarm.js
@@ -1,4 +1,208 @@
-//default impl TCP
-var iAlarmDefault = require('./ialarm-tcp');
 
-module.exports = iAlarmDefault
+const MeianSocket = require('./src/meian-socket');
+const alarmStatus = require('./src/status-decoder')();
+
+function MeianClient(host, port, uid, pwd) {
+
+    var self = this;
+
+    const socket = MeianSocket(host, port, uid, pwd);
+
+    /**
+     * Full status: armed/disarmed/triggered and all sensors data with names
+     * @param {*} forceZoneInfo 
+     */
+    self.getStatus = function () {
+        return new Promise((resolve, reject) => {
+            const commands = ['GetAlarmStatus', 'GetByWay', 'GetZone'];
+            return socket.executeCommand(commands).then(function ({ data }) {
+
+                const { GetAlarmStatus, GetByWay, GetZone } = data
+
+                if (!GetAlarmStatus) {
+                    reject("GetAlarmStatus returned empty data");
+                }
+                if (!GetByWay) {
+                    reject("GetByWay returned empty data");
+                }
+                if (!GetZone) {
+                    reject("GetZone returned empty data");
+                }
+
+                var response = {
+                    status: GetAlarmStatus,
+                    zones: GetByWay.zones
+                };
+
+                const zoneConfig = GetZone && GetZone.zones;
+                if (response.zones && zoneConfig) {
+                    for (let index = 0; index < response.zones.length; index++) {
+                        const zone = response.zones[index];
+                        const info = zoneConfig.find(z => z.id === zone.id);
+                        //merge
+                        response.zones[index] = {
+                            ...zone,
+                            ...info
+                        }
+                    }
+                }
+
+                resolve(response);
+            }, function (err) {
+                reject(err);
+            })
+        });
+    }
+
+    /**
+     * Arm Away
+     */
+    self.armAway = function () {
+        return self.armDisarm('ARMED_AWAY');
+    }
+
+    /**
+     * Arm home
+     */
+    self.armHome = function () {
+        return self.armDisarm('ARMED_HOME');
+    };
+
+
+    /**
+     * Arm home alias
+     */
+    self.armStay = function () {
+        return self.armHome();
+    };
+
+
+    /**
+     * Disarm
+     */
+    self.disarm = function () {
+        return self.armDisarm('DISARMED');
+    };
+
+    /**
+     * Cancel triggered status
+     */
+    self.cancel = function () {
+        return self.armDisarm('CANCEL');
+    };
+
+    /**
+     * promise with command execution and full status response
+     * @param {*} commands 
+     * @param {*} args 
+     * @returns 
+     */
+    function MeianPromiseWithStatusResponse(commands, args) {
+        return new Promise((resolve, reject) => {
+            console.log("requesting " + commands + " with args " + args)
+            socket.executeCommand(commands, args).then(function (commandResponse) {
+                console.log(commandResponse);
+                self.getStatus().then(function (response) {
+                    if (response) {
+                        resolve(response);
+                    } else {
+                        reject("getStatus returned no data");
+                    }
+                }, function (err) {
+                    reject(err);
+                })
+            }, function (err) {
+                reject(err);
+            });
+        });
+    }
+
+    /**
+     * generic arm disarm function
+     * @param {*} requestedArmedStatus 
+     * @returns 
+     */
+    self.armDisarm = function (requestedArmedStatus) {
+        const value = alarmStatus.fromStatusToTcpValue(requestedArmedStatus);
+        return MeianPromiseWithStatusResponse(['SetAlarmStatus'], [value]);
+    }
+
+    /**
+     * Bypass/reset bypass zone
+     * @param {*} number 
+     * @param {*} bypassed 
+     */
+    self.bypassZone = function (zoneNumber, bypassed) {
+        console.log("bypass zone " + zoneNumber + "=" + bypassed)
+        //in tcp call zone is Zero-based numbering
+        return MeianPromiseWithStatusResponse(['SetByWay'], [[zoneNumber - 1, bypassed]]);
+    }
+
+    /**
+     * Get all zones definition
+     */
+    self.getZoneInfo = function (zoneNumber) {
+        return new Promise((resolve, reject) => {
+            socket.executeCommand(['GetZone']).then(function (response) {
+                if (response && response.data && response.data.GetZone) {
+                    var zones = response.data.GetZone.zones;
+                    if (zoneNumber) {
+                        const info = zones.find(z => z.id === zoneNumber);
+                        if (info) {
+                            return info;
+                        }
+                    }
+                    resolve(zones);
+                } else {
+                    reject("GetZone returned no data");
+                }
+            }, function (err) {
+                reject(err);
+            });
+        });
+    }
+
+    /**
+     * Logged events (armed, disarmed, etc)
+     * @returns 
+     */
+    self.getEvents = function () {
+        return new Promise((resolve, reject) => {
+            socket.executeCommand(['GetLog']).then(function ({ data }) {
+                if (data && data.GetLog && data.GetLog.logs) {
+                    resolve(data.GetLog.logs);
+                } else {
+                    reject("GetLog returned no data");
+                }
+            }, function (err) {
+                reject(err);
+            });
+        });
+    };
+
+    /**
+     * same as getZoneInfo without any zoneNumber
+     * @returns 
+     */
+    self.getAllZones = function () {
+        return self.getZoneInfo();
+    }
+
+    /**
+     * filter zones with relevant status 
+     * @param {*} zones 
+     * @returns 
+     */
+    self.filterStatusZones = function (zones) {
+        if (!zones) {
+            return [];
+        }
+        return zones.filter(function (zone) {
+            return zone.status > 1;
+        });
+    };
+
+    return self;
+}
+
+module.exports = MeianClient;
